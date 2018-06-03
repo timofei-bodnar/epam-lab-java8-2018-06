1cfd764962cce539ce28e1bc6537868457f5bb7f
golantr <golant_a@rambler.ru>
2018-06-03 16:40:56 +0300

    exercise3

---
+++ src/test/java/lambda/part1/exercise/Exercise3.java
@@ -0,0 +1,65 @@
+package lambda.part1.exercise;
+
+import com.google.common.collect.FluentIterable;
+import lambda.data.Person;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+@SuppressWarnings({"ConstantConditions", "unused"})
+public class Exercise3 {
+
+    @Test
+    public void sortPersonsByAgeUsingArraysSortExpressionLambda() {
+        Person[] persons = getPersons();
+
+        Arrays.sort(persons, (p1, p2) -> Integer.compare(p1.getAge(), p2.getAge()));
+
+        assertArrayEquals(new Person[]{
+            new Person("Иван", "Мельников", 20),
+            new Person("Николай", "Зимов", 30),
+            new Person("Алексей", "Доренко", 40),
+            new Person("Артем", "Зимов", 45)
+        }, persons);
+    }
+
+    @Test
+    public void sortPersonsByLastNameThenFirstNameUsingArraysSortExpressionLambda() {
+        Person[] persons = getPersons();
+
+        Arrays.sort(persons, (p1, p2) ->
+                !p1.getLastName().equals(p2.getLastName()) ?
+                        p1.getLastName().compareTo(p2.getLastName()) :
+                        p1.getFirstName().compareTo(p2.getFirstName())
+        );
+
+        assertArrayEquals(new Person[]{
+            new Person("Алексей", "Доренко", 40),
+            new Person("Артем", "Зимов", 45),
+            new Person("Николай", "Зимов", 30),
+            new Person("Иван", "Мельников", 20)
+        }, persons);
+    }
+
+    @Test
+    public void findFirstWithAge30UsingGuavaPredicateLambda() {
+        List<Person> persons = Arrays.asList(getPersons());
+
+        Person person = FluentIterable.from(persons).firstMatch(p -> p.getAge() == 30).orNull();
+
+        assertEquals(new Person("Николай", "Зимов", 30), person);
+    }
+
+    private Person[] getPersons() {
+        return new Person[]{
+            new Person("Иван", "Мельников", 20),
+            new Person("Алексей", "Доренко", 40),
+            new Person("Николай", "Зимов", 30),
+            new Person("Артем", "Зимов", 45)
+        };
+    }
+}
