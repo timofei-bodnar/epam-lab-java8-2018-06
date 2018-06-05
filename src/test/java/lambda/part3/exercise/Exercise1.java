42fb81b7b9cc2f4acde04f0e5b42745eadfaadb2
Andrewisnew <zakomornii1998@gmail.com>
2018-06-05 23:33:55 +0300

    completed part3.exercise1

--- src/test/java/lambda/part3/exercise/Exercise1.java
+++ src/test/java/lambda/part3/exercise/Exercise1.java
@@ -1,5 +1,7 @@
 package lambda.part3.exercise;
 
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import lambda.data.Employee;
 import lambda.data.JobHistoryEntry;
 import lambda.data.Person;
@@ -20,55 +22,56 @@ public class Exercise1 {
         List<Employee> employees = getEmployees();
         List<Integer> lengths = new ArrayList<>();
 
-        // TODO функция извлечения информации о человеке из объекта сотрудника personExtractor: Employee -> Person
-        // TODO функция извлечения полного имени из информации о человеке fullNameExtractor: Person -> String
-        // TODO функция извлечения длины из строки stringLengthExtractor: String -> Integer
-        // TODO функция извлечения длины полного имени из сотрудника fullNameLengthExtractor: Employee -> Integer
-        // TODO преобразование списка employees в lengths используя fullNameLengthExtractor
+        Function<Employee, Person> personExtractor = Employee::getPerson;
+        Function<Person, String> fullNameExtractor = Person::getFullName;
+        Function<String, Integer> stringLengthExtractor = String::length;
+        Function<Employee, Integer> fullNameLengthExtractor = personExtractor
+            .andThen(fullNameExtractor).andThen(stringLengthExtractor);
+        lengths = employees.stream().map(fullNameLengthExtractor).collect(Collectors.toList());
 
         assertEquals(Arrays.asList(14, 19, 14, 15, 14, 16), lengths);
     }
 
     public static List<Employee> getEmployees() {
         return Arrays.asList(
-                new Employee(
-                        new Person("Иван", "Мельников", 30),
-                        Arrays.asList(
-                                new JobHistoryEntry(2, "dev", "EPAM"),
-                                new JobHistoryEntry(1, "dev", "google")
-                        )),
-                new Employee(
-                        new Person("Александр", "Дементьев", 28),
-                        Arrays.asList(
-                                new JobHistoryEntry(1, "tester", "EPAM"),
-                                new JobHistoryEntry(1, "dev", "EPAM"),
-                                new JobHistoryEntry(1, "dev", "google")
-                        )),
-                new Employee(
-                        new Person("Дмитрий", "Осинов", 40),
-                        Arrays.asList(
-                                new JobHistoryEntry(3, "QA", "yandex"),
-                                new JobHistoryEntry(1, "QA", "mail.ru"),
-                                new JobHistoryEntry(1, "dev", "mail.ru")
-                        )),
-                new Employee(
-                        new Person("Анна", "Светличная", 21),
-                        Collections.singletonList(
-                                new JobHistoryEntry(1, "tester", "T-Systems")
-                        )),
-                new Employee(
-                        new Person("Игорь", "Толмачёв", 50),
-                        Arrays.asList(
-                                new JobHistoryEntry(5, "tester", "EPAM"),
-                                new JobHistoryEntry(6, "QA", "EPAM")
-                        )),
-                new Employee(
-                        new Person("Иван", "Александров", 33),
-                        Arrays.asList(
-                                new JobHistoryEntry(2, "QA", "T-Systems"),
-                                new JobHistoryEntry(3, "QA", "EPAM"),
-                                new JobHistoryEntry(1, "dev", "EPAM")
-                        ))
+            new Employee(
+                new Person("Иван", "Мельников", 30),
+                Arrays.asList(
+                    new JobHistoryEntry(2, "dev", "EPAM"),
+                    new JobHistoryEntry(1, "dev", "google")
+                )),
+            new Employee(
+                new Person("Александр", "Дементьев", 28),
+                Arrays.asList(
+                    new JobHistoryEntry(1, "tester", "EPAM"),
+                    new JobHistoryEntry(1, "dev", "EPAM"),
+                    new JobHistoryEntry(1, "dev", "google")
+                )),
+            new Employee(
+                new Person("Дмитрий", "Осинов", 40),
+                Arrays.asList(
+                    new JobHistoryEntry(3, "QA", "yandex"),
+                    new JobHistoryEntry(1, "QA", "mail.ru"),
+                    new JobHistoryEntry(1, "dev", "mail.ru")
+                )),
+            new Employee(
+                new Person("Анна", "Светличная", 21),
+                Collections.singletonList(
+                    new JobHistoryEntry(1, "tester", "T-Systems")
+                )),
+            new Employee(
+                new Person("Игорь", "Толмачёв", 50),
+                Arrays.asList(
+                    new JobHistoryEntry(5, "tester", "EPAM"),
+                    new JobHistoryEntry(6, "QA", "EPAM")
+                )),
+            new Employee(
+                new Person("Иван", "Александров", 33),
+                Arrays.asList(
+                    new JobHistoryEntry(2, "QA", "T-Systems"),
+                    new JobHistoryEntry(3, "QA", "EPAM"),
+                    new JobHistoryEntry(1, "dev", "EPAM")
+                ))
         );
     }
 
